defdeeb2243252eba750a9e2487c357e011b8ac9
Y. H. Wang <34644391+KyleWang12@users.noreply.github.com>
2023-04-08 14:17:23 -0700

    add batch write

---
+++ assignment3/consumer_batch_write/src/main/java/constants/DatabaseConstants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class DatabaseConstants {
+  public static final String host = "172.31.31.21";
+  public static final int port = 6379;
+  public static final String password = "wyhadmin";
+  public static final int timeout = 5000;
+}
